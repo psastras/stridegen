d574b494003db9b75da27112f4d080c9f149ae5c
Paul Sastrasinh <psastras@gmail.com>
2016-06-26 14:11:23 -0400

    Publish Version

--- ci/github-page.sh
+++ ci/github-page.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -x -e
 git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b gh-pages public
-ls dist
 cp -rf dist/* public/
 cd public
 git config --global user.email "psastras@gmail.com"
