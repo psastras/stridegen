b8acce63b2d2a3448a17142cbf30835fe60db79b
Paul Sastrasinh <psastras@gmail.com>
2016-06-26 13:31:17 -0400

    Update gh-pages script

--- ci/github-page.sh
+++ ci/github-page.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x -e
 git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b gh-pages public
-cp dist/* public/
+cp -rf dist/* public/
 cd public
 git config --global user.email "psastras@gmail.com"
 git config --global user.name $CIRCLE_PROJECT_USERNAME
