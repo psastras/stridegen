98693db796755fcc67de517bc46897412feb7fc0
Paul Sastrasinh <psastras@gmail.com>
2016-06-26 13:02:11 -0400

    Test Add gh-pages

---
+++ ci/github-page.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x -e
+git clone git@github.com:$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git -b gh-pages public
+cp dist/* public/
+cd public
+git config --global user.email "psastras@gmail.com"
+git config --global user.name $CIRCLE_PROJECT_USERNAME
+git add .
+git commit -m "publi:ship: $CIRCLE_BUILD_NUM"
+git push origin gh-pages
